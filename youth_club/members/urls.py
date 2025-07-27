e1dc24476712e8736fc47385ce188e2bda995c3b
dhailu <106366108+dhailu@users.noreply.github.com>
2025-07-27 04:42:19 -0700

    master template added

--- youth_club/members/urls.py
+++ youth_club/members/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
 from . import views
 
+
+## First version URL
+# urlpatterns = [
+#     path('members/', views.members, name='members'),
+# ]
+
+## Note about detail URL
+# Now we need to make sure that the /details/ url points to the correct view, with id as a parameter.
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
 ]
