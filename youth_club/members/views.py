9e312b697567ab4b6f16ba0a767390587f94ad79
dhailu <106366108+dhailu@users.noreply.github.com>
2025-07-27 04:07:27 -0700

    html file updated

--- youth_club/members/views.py
+++ youth_club/members/views.py
@@ -1,6 +1,26 @@
+
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
+## Note about the view 
+# The members view does the following:
+# Creates a mymembers object with all the values of the Member model.
+# Loads the all_members.html template.
+# Creates an object containing the mymembers object.
+# Sends the object to the template.
+# Outputs the HTML that is rendered by the template.
+
+## First version
+# def members(request):
+#   template = loader.get_template('myfirst.html')
+#   return HttpResponse(template.render())
+
+## Second version
 def members(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
